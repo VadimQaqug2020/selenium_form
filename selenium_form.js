f6c641130c3834edebf257c47596b6365a54e102
VadimQaqug2020 <sawchenko.vadim@gmail.com>
2020-10-27 18:26:05 +0200

    test commit

--- selenium_form.js
+++ selenium_form.js
@@ -1,6 +1,32 @@
 // Open https://the-internet.herokuapp.com/login
 
+/// <reference types="cypress" />
+
 // Please automate next test cases:
 // 1. Login with valid creds (tomsmith/SuperSecretPassword!) and assert you successfully logged in
+it('Login valid', () => {
+    cy.visit('https://the-internet.herokuapp.com/login') 
+    cy.get('#username').type('tomsmith')
+    cy.get('#password').type('SuperSecretPassword!')
+    cy.get('[type="submit"]').click()
+    cy.get('[class="subheader"]').contains('Welcome').should('exist')
+
+});
 // 2. Login with invalid creds and check validation error
+it('Login invalid', () => {
+    cy.visit('https://the-internet.herokuapp.com/login') 
+    cy.get('#username').type('wrong')
+    cy.get('#password').type('wrong!')
+    cy.get('[type="submit"]').click()
+    cy.get('[class="flash error"]').contains('invalid').should('exist')
+});
 // 3. Logout from app and assert you successfully logged out
+it('Login valid', () => {
+    cy.visit('https://the-internet.herokuapp.com/login') 
+    cy.get('#username').type('tomsmith')
+    cy.get('#password').type('SuperSecretPassword!')
+    cy.get('[type="submit"]').click()
+    cy.get('[class="button secondary radius"]').click()
+    cy.get('[class="flash success"]').contains('logged out').should('exist')
+
+});
